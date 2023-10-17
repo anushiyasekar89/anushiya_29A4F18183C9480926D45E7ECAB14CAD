6eeb11451c577a9827e235699fc5d7a7e02ba07f
anushiyasekar89 <147534126+anushiyasekar89@users.noreply.github.com>
2023-10-17 04:36:23 -0700

    Add files via upload

---
+++ main (7).py
@@ -0,0 +1,19 @@
+mplement a class called Player that represents a cricket player. The Player class should have a method called play() which prints 
+# Define the Player class
+class Player:
+    def play(self):
+        print("The player is playing cricket.")
+# Define the Batsman class, derived from Player
+class Batsman(Player):
+    def play(self):
+        print("The batsman is batting.")
+# Define the Bowler class, derived from Player
+class Bowler(Player):
+    def play(self):
+        print("The bowler is bowling.")
+# Create objects of Batsman and Bowler classes
+batsman = Batsman()
+bowler = Bowler()
+# Call the play() method for each object
+batsman.play()
+bowler.play()
